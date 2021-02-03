3bd497e5cf249843aadaa3fcc4172ccd4133db05
Tanyiqu <1953649096@qq.com>
2021-02-03 11:35:33 +0800

    登录页面 基本布局

---
+++ src/views/Login.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="login">
+    <el-form
+      class="login-form"
+      label-position="left"
+      label-width="80px"
+      :model="formData"
+    >
+      <h2 class="title">管理员登录</h2>
+      <el-form-item label="账号：" label-width="70px">
+        <el-input v-model="formData.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码：" label-width="70px">
+        <el-input v-model="formData.password" type="password"></el-input>
+      </el-form-item>
+
+      <el-button class="btn-login" type="primary">登录</el-button>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      formData: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.login {
+  width: 100%;
+  height: 100%;
+  background: #27825d;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .login-form {
+    background: #fff;
+    padding: 25px 35px;
+    border-radius: 12px;
+
+    .title {
+      margin-bottom: 22px;
+    }
+
+    .btn-login {
+      width: 100%;
+    }
+  }
+}
+</style>
